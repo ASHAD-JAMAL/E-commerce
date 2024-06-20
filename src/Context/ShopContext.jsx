cdd30a4aa056ad73f7a732ba1e786fe48dd7a054
Ashad Jamal <ashadjamalhyt@gmail.com>
2024-06-20 13:44:29 +0530

    responsive ness

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -29,12 +29,29 @@ const ShopContextProvider = (props) => {
         let itemInfo = all_product.find(
           (product) => product.id === Number(item)
         );
-        totalAmount +=  cartItems[item]*itemInfo.new_price;
+        totalAmount += cartItems[item] * itemInfo.new_price;
       }
     }
     return totalAmount;
   };
-  const contextValue = {getTotalCartAmount, all_product, cartItems, addToCart, removeFromCart };
+
+  const getTotalCartItems = () => {
+    let totalItem = 0;
+    for (const item in cartItems) {
+      if (cartItems[item] > 0) {
+        totalItem += cartItems[item];
+      }
+    }
+    return totalItem;
+  };
+  const contextValue = {
+    getTotalCartItems,
+    getTotalCartAmount,
+    all_product,
+    cartItems,
+    addToCart,
+    removeFromCart,
+  };
   return (
     <ShopContext.Provider value={contextValue}>
       {props.children}
